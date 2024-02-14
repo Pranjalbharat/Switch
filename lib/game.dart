2037ff33b64b2705a3ed44d47b5fe825189f081d
Pranjal <pranjalbharat07@gmail.com>
2024-02-14 19:55:38 +0530

    bulb added and other pos fixes

--- lib/game.dart
+++ lib/game.dart
@@ -5,6 +5,7 @@ import 'package:flame/game.dart';
 import 'package:switch_off/components/background.dart';
 import 'package:switch_off/components/bed.dart';
 import 'package:switch_off/components/borderline.dart';
+import 'package:switch_off/components/bulb.dart';
 
 import 'package:switch_off/components/door.dart';
 import 'package:switch_off/components/screen.dart';
@@ -16,24 +17,29 @@ class SwitchGame extends FlameGame{
   late Screen screen;
   late BorderLine borderline;
   late Door door;
+  late Bulb bulb;
 
   
     @override
   Future<void> onLoad() async {
     await super.onLoad();
-   
+  
   background=Background();
   add(background);
-   screen=Screen();
-    add(screen);
+  screen=Screen();
+  add(screen);
 borderline=BorderLine();
 add(borderline);
+  // bulb=Bulb();
+  // add(bulb);
 door=Door();
 add(door);
 
-    bed=Bed();
+  bed=Bed();
   add(bed);
- 
+  bulb=Bulb();
+  add(bulb);
+
     startDayNightCycle();
   }
 
@@ -42,6 +48,7 @@ add(door);
       bed.toggleDayNight();
       background.toggleDayNight(); 
       door.toggleDayNight();
+      bulb.toggleDayNight();
      // Toggle between day and night every 5 seconds
     });
   }

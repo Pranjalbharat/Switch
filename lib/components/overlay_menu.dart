aa74a7a5f3ab29cc5aec1d1aa125afed658c479b
Saloni <sgoel0024@gmail.com>
2024-02-21 18:57:05 +0530

    overlay

--- lib/components/overlay_menu.dart
+++ lib/components/overlay_menu.dart
@@ -167,31 +167,40 @@ class _OverlayMenuPageState extends State<OverlayMenuPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Colors.orange[100],
       body: Center(
         child: Container(
-          padding: EdgeInsets.all(20.0),
-          width: MediaQuery.of(context).size.width * 0.8, // 80% of screen width
+          
+          padding: EdgeInsets.all(40.0),
+          width: MediaQuery.of(context).size.width * 0.6, // 80% of screen width
           decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20.0),
+            color: Colors.orange[300],
+            borderRadius: BorderRadius.circular(50.0),
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
+            
             children: [
-              Text(
+              MainText(
+                text:
                 'Your Score',
-                style: TextStyle(
-                  fontSize: 24.0,
+                 //backgroundColor: Colors.red, // Background color of the bow
+            textColor: Colors.white, // Text color
+            height: 80, // Adjust the height of the bow shape
+            width: 1000,
+                /*style: TextStyle(
+                  fontSize: 28.0,
                   fontWeight: FontWeight.bold,
-                ),
+                  color: Colors.red
+                ),*/
               ),
               SizedBox(height: 20.0),
               Text(
                 'Score: ${widget.score}',
                 style: TextStyle(
-                  fontSize: 20.0,
+                  fontSize: 27.0,
+                  color:Colors.red[700] 
                 ),
               ),
               SizedBox(height: 40.0),
@@ -199,19 +208,19 @@ class _OverlayMenuPageState extends State<OverlayMenuPage> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   CircularButton(
-                    image: AssetImage('menu.png'),
+                    image: AssetImage('assets/images/menu.png'),
                     onPressed: () {
                       // Add onPressed logic for the first button
                     },
                   ),
                   CircularButton(
-                    image: AssetImage('cart.png'),
+                    image: AssetImage('assets/images/cart.png'),
                     onPressed: () {
                       // Add onPressed logic for the second button
                     },
                   ),
                   CircularButton(
-                    image: AssetImage('arrow.png'),
+                    image: AssetImage('assets/images/arrow (1).png'),
                     onPressed: () {
                       // Add onPressed logic for the third button
                     },
@@ -240,15 +249,58 @@ class CircularButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
-        shape: CircleBorder(), backgroundColor: Colors.blue,
+        shape: CircleBorder(), backgroundColor: Colors.yellow[900],
         padding: EdgeInsets.all(15.0),
       ),
       child: CircleAvatar(
-        radius: 30.0,
+        radius: 40.0,
         backgroundImage: image,
       ),
     );
   }
 }
 
+class MainText extends StatelessWidget {
+  final String text;
+  final Color textColor;
+  final double height;
+  final double width;
 
+  const MainText({
+    Key? key,
+    required this.text,
+    required this.textColor,
+    required this.height,
+    required this.width,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.orange.shade300, Colors.red, Colors.orange.shade400],
+        ),
+        borderRadius: BorderRadius.circular(height / 2),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 1,
+            blurRadius: 2,
+            offset: Offset(0, 2), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(color: textColor, fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
